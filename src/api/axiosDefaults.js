8ca306f6dd58be489bd2f831c6696f030c67d91f
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2025-02-25 14:05:45 +0000

    update to navbar to show logged in titles and icon. avatar for profile added

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,8 @@ axios.defaults.baseURL = 'https://react-p5-test-3e9d984aefe4.herokuapp.com/'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// page refresh
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
